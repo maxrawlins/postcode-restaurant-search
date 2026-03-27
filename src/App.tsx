615be954a784a452b7cd3fca0de121508c055e1b
Max Rawlins <maxrawlins@hotmail.co.uk>
2026-03-27 10:53:36 +0000

    added postcode search form layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+
 function App() {
+  const [postcode, setPostcode] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    //prevents page refresh
+    e.preventDefault();
+
+    console.log(postcode);
+  };
+
   return (
     <main>
       <h1>Postcode Restaurant Search</h1>
+      
+      <p>Search for restaurants by UK postcode</p>
+
+      <form onSubmit={handleSubmit}> 
+        <input type = "text" 
+        placeholder =  "Enter a UK postcode"
+        value = {postcode}
+        onChange={(e) => setPostcode(e.target.value)}
+        />
+        <button type = "submit">Search</button>
+      </form>
+
+      <section>
+      <h2>Results</h2>
+      
+      </section>
     </main>
   );
 }
+
+
 
 export default App;
